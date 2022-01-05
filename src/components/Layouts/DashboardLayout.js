551497af0453b6adca0d6df5c91ad177912bbff6
Gigman <abbeykojoeric@gmail.com>
2022-01-05 14:34:42 +0000

    small update to ui

--- src/components/Layouts/DashboardLayout.js
+++ src/components/Layouts/DashboardLayout.js
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 import './Dashboard.css'
 import MenuDown from '@material-ui/icons/ArrowDropDown'
+import MenuUp from '@material-ui/icons/ArrowDropUp'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import SupervisorAccount from '@material-ui/icons/SupervisorAccount'
+// import Output from '@material-ui/icons/Output'
 
 const DashboardLayout = ({children}) => {
+    const [open, setOpen] = useState(false)
     return (
         <div className='dashboard-layout'>
             <div className="navbar">
@@ -9,8 +15,13 @@ const DashboardLayout = ({children}) => {
                 <div className="navbar-user">
                     <div className="avatar-box"></div>
                     <div>Xanthe Neal</div>
-                    <MenuDown />
+                    <div style={{cursor: 'pointer', marginTop: '8px'}} onClick={() => setOpen(!open)}>{open ? <MenuUp /> : <MenuDown />}</div>
                 </div>
+               {open &&  <div className='avatar-box-dialog'>
+                    <div className='item active'> <AccountCircle /> <span>My Profile</span></div>
+                    <div className='item'><SupervisorAccount/> <span>Group Chat</span></div>
+                    <div className='item'> <span>Logout</span></div>
+                </div>}
             </div>
             {children}
         </div>
